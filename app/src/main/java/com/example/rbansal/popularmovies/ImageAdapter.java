54b147e52a7bce5c37f7100b8d6eadfce7d2dd2c
rbansalrahul6 <rbansalrahul6@gmail.com>
2016-05-27 16:23:23 +0530

    text under images

--- app/src/main/java/com/example/rbansal/popularmovies/ImageAdapter.java
+++ app/src/main/java/com/example/rbansal/popularmovies/ImageAdapter.java
@@ -1,64 +1,76 @@
 package com.example.rbansal.popularmovies;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * Created by rbansal on 27/5/16.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class ImageAdapter extends BaseAdapter {
-    private Context mContext;
+    private final List<Item> mItems = new ArrayList<Item>();
+    private final LayoutInflater mInflater;
 
-    public ImageAdapter(Context c) {
-        mContext = c;
+    public ImageAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+
+        mItems.add(new Item("BIRDMAN",       R.drawable.sample_0));
+        mItems.add(new Item("AMERICAN BEAUTY",   R.drawable.sample_1));
+        mItems.add(new Item("RAIN MAN", R.drawable.sample_2));
+        mItems.add(new Item("GANDHI",      R.drawable.sample_3));
+        mItems.add(new Item("SAMPLE",     R.drawable.sample_4));
+        mItems.add(new Item("THE GREATEST SHOW ON EARTH",      R.drawable.sample_5));
     }
 
+    @Override
     public int getCount() {
-        return mThumbIds.length;
+        return mItems.size();
     }
 
-    public Object getItem(int position) {
-        return null;
+    @Override
+    public Item getItem(int i) {
+        return mItems.get(i);
     }
 
-    public long getItemId(int position) {
-        return 0;
+    @Override
+    public long getItemId(int i) {
+        return mItems.get(i).drawableId;
     }
 
-    // create a new ImageView for each item referenced by the Adapter
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if (convertView == null) {
-            // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
-           imageView.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-            imageView.setAdjustViewBounds(true);
-           // imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-           // imageView.setPadding(8, 8, 8, 8);
-        } else {
-            imageView = (ImageView) convertView;
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View v = view;
+        ImageView picture;
+        TextView name;
+
+        if (v == null) {
+            v = mInflater.inflate(R.layout.grid_item, viewGroup, false);
+            v.setTag(R.id.picture, v.findViewById(R.id.picture));
+            v.setTag(R.id.text, v.findViewById(R.id.text));
         }
 
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
+        picture = (ImageView) v.getTag(R.id.picture);
+        name = (TextView) v.getTag(R.id.text);
+
+        Item item = getItem(i);
+        picture.setAdjustViewBounds(true);
+        picture.setImageResource(item.drawableId);
+        name.setText(item.name);
+
+        return v;
     }
 
-    // references to our images
-    private Integer[] mThumbIds = {
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7,
-            R.drawable.sample_0, R.drawable.sample_1,
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7,
-            R.drawable.sample_0, R.drawable.sample_1,
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7
-    };
+    private static class Item {
+        public final String name;
+        public final int drawableId;
+
+        Item(String name, int drawableId) {
+            this.name = name;
+            this.drawableId = drawableId;
+        }
+    }
 }
