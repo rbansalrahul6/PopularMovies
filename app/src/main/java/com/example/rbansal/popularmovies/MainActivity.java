8aa96f5095429cd463dfacea9b31ee98f0656719
rbansalrahul6 <rbansalrahul6@gmail.com>
2016-05-29 12:05:41 +0530

    json parsing tested

--- app/src/main/java/com/example/rbansal/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/rbansal/popularmovies/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rbansal.popularmovies;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,10 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.GridView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -66,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_refresh) {
             FetchMovie movie = new FetchMovie();
-            movie.execute();
+            movie.execute("top_rated");
             return true;
         }
 
@@ -75,26 +80,64 @@ public class MainActivity extends AppCompatActivity {
 
     //image adapter class
 
-    public class FetchMovie extends AsyncTask<Void, Void, Void> {
+    public class FetchMovie extends AsyncTask<String, Void, Movie[]> {
+        private final String LOG_TAG = FetchMovie.class.getSimpleName();
+        //method for extracting data from json string
+        private Movie[] getMovieDataFromJson(String jsonStr)
+        throws JSONException {
+            //name of json items neede
+            final String TITLE = "title";
+            final String POSTER_PATH = "poster_path";
+            final String PAGE_NO = "page";
+            final String TOTAL_PAGES = "total_pages";
+            final String TOTAL_RESULTS = "total_results";
+            final String RESULTS = "results";
+            JSONObject movieJson = new JSONObject(jsonStr);
+            JSONArray resultArray = movieJson.getJSONArray(RESULTS);
+            Movie[] resultMovies = new Movie[resultArray.length()];
+
+            for(int i=0;i<resultArray.length();i++) {
+                JSONObject movie = resultArray.getJSONObject(i);
+                resultMovies[i] = new Movie(movie.getString(POSTER_PATH), movie.getString(TITLE));
+            }
+            for(Movie m : resultMovies) {
+                Log.v(LOG_TAG, "TITLE: "+m.name+" Path: "+m.imgUrl+"\n");
+            }
+            return  resultMovies;
+        }
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Movie[] doInBackground(String... params) {
+            if(params.length == 0)
+            {
+                return  null;
+            }
             //making http request
             // These two need to be declared outside the try/catch
-// so that they can be closed in the finally block.
+            // so that they can be closed in the finally block.
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
 
-// Will contain the raw JSON response as a string.
-            String forecastJsonStr = null;
+            // Will contain the raw JSON response as a string.
+            String movieJsonStr = null;
+            //values of parametres to be passed in url
+            String category = "popular";
+            String api_key = "619641613cb9bf7bbfec1d8abbcc63da";
+            int page = 1;
 
             try {
-                // Construct the URL for the OpenWeatherMap query
-                // Possible parameters are avaiable at OWM's forecast API page, at
-                // http://openweathermap.org/API#forecast
-                URL url = new URL("https://api.themoviedb.org/3/movie/550?api_key=619641613cb9bf7bbfec1d8abbcc63da");
-
-                // Create the request to OpenWeatherMap, and open the connection
+                //parametres to be passed in url
+                String base_url = "http://api.themoviedb.org/3/movie/";
+                final String API_KEY = "api_key" ;
+                final String PAGE_PARAM = "page";
+                //building final url
+                base_url = base_url + params[0] + "?";
+                Uri builtUri = Uri.parse(base_url).buildUpon()
+                        .appendQueryParameter(API_KEY, api_key)
+                        .appendQueryParameter(PAGE_PARAM, Integer.toString(page))
+                        .build();
+                URL url = new URL(builtUri.toString());
+                // Create the request
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
                 urlConnection.connect();
@@ -120,8 +163,7 @@ public class MainActivity extends AppCompatActivity {
                     // Stream was empty.  No point in parsing.
                     return null;
                 }
-                forecastJsonStr = buffer.toString();
-                Log.v("json",forecastJsonStr);
+                movieJsonStr = buffer.toString();
             } catch (IOException e) {
                 Log.e("PlaceholderFragment", "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attemping
@@ -139,6 +181,14 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             }
+            try {
+                return getMovieDataFromJson(movieJsonStr);
+
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+            //this will happen only if there is an error ehile parsing data
             return null;
         }
     }
